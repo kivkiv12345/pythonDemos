1a157e7cce41f6cffd7c71c24d2bc6807c02e818
kivkiv12345 <57446369+kivkiv12345@users.noreply.github.com>
2021-09-08 05:03:41 +0200

    Added support for Union typehints

--- overloading.py
+++ overloading.py
@@ -1,26 +1,8 @@
 """ This module demonstrates a decorator which adds support for traditional function/method overloading in Python. """
 
 import time
-from typing import Callable, Any
+from typing import Callable, Any, Union, get_args
 import inspect
-
-
-# def test(a, b):
-#     pass
-#
-# def test2(b=2):
-#     pass
-#
-# def test3(c: int):
-#     pass
-#
-# def test4(d: str = None):
-#     pass
-#
-# sig1 = tuple(inspect.signature(test).parameters.values())
-# sig2 = tuple(inspect.signature(test2).parameters.values())
-# sig3 = tuple(inspect.signature(test3).parameters.values())
-# sig4 = tuple(inspect.signature(test4).parameters.values())
 
 
 def overload(func: Callable, /, _function_cache={}) -> Callable:
@@ -28,8 +10,6 @@ def overload(func: Callable, /, _function_cache={}) -> Callable:
     Decorator which adds support for traditional function overloading.
     :param func: Function which should be overloadable.
     """
-
-    # TODO Kevin: Adds support for typing typehints, such as Union.
 
     targets = _function_cache.get(func.__qualname__, {})
 
@@ -71,8 +51,9 @@ def overload(func: Callable, /, _function_cache={}) -> Callable:
                 # Iterate over the signature and arguments simultaneously to check that their types are compatible
                 for param1, param2 in zip(signature, params):
                     # Ignore the type of the argument, if the parameter accepts any type.
-                    # Otherwise; check that they match.
-                    if not (param1 is Any or param1 is param2):
+                    # Otherwise; check that they match. If they don't, the parameter may be a Union,
+                    # in which case we check if it contains the type of the argument.
+                    if not (param1 is Any or (param1 is param2 or param2 in get_args(param1))):
                         break  # Break and check the signature of the next function.
                 else:  # Will run when the loop finished without hitting break; meaning we found a compatible signature.
                     return function(*args, **kwargs)
@@ -83,54 +64,18 @@ def overload(func: Callable, /, _function_cache={}) -> Callable:
     return wrapper
 
 
-class Test:
-
-    @overload
-    def test(self, a: int):
-        print("Running method with int")
-
-    @overload
-    def test(self, b: str):
-        print("Running method with string")
-
-
 @overload
-def overload_test(a: int, b=''):
-    print('running with int')
-
-
-@overload
-def overload_test(a: str):
-    print('running with string')
-
-
-@overload
-def another_function(a: str):
-    print('running another function with string')
-
-
-@overload
-def kwargtest(a: str):
-    pass
-
-
-@overload
-def kwargtest(a: str, b: int, c=3):
+def uniontest(*args):
     pass
 
 
 if __name__ == '__main__':
 
-    # temp = Test()
-    # temp.test(3)
-    # temp.test('3')
-    #
-    # overload_test('hej')
-    # another_function('')
+    #test = tuple(inspect.signature(uniontest).parameters.values())
 
     before = time.time()
 
     for _ in range(1000000):
-        kwargtest('a')
+        uniontest(3.3, 'a', 3)
 
     print(f"Took: {time.time() - before}")
